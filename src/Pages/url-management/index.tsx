9203a29fbfea0da488463a5542cecfd727640ae3
Olumide Oluwole <oluwoleit@gmail.com>
2025-07-13 19:24:01 +0200

    chore: replaced native websocket with react-use-websocket to handle consistent disconnect

--- src/Pages/url-management/index.tsx
+++ src/Pages/url-management/index.tsx
@@ -1,14 +1,16 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Tooltip, Typography } from "@mui/material";
 import { URLForm } from "./components/URLForm";
-import { useWebsocket } from "@/hooks/useWebsocket";
 import { useState } from "react";
 import { crawlAPIRequest } from "@/api/url-management";
 import { toast } from "react-toastify";
 import { getErrorMessage } from "@/utils";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { useReactWebSocket } from "@/hooks/useReactWebSocket";
 
 const URLManagement = () => {
-  const { websocketInstance, isConnected, socketData } = useWebsocket();
+  // const { websocketInstance, isConnected, socketData } = useWebsocket();
+  const { connectionStatus, getWebSocket, lastJsonMessage, socketData } =
+    useReactWebSocket();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [jobID, setJobID] = useState<string>("");
 
@@ -30,27 +32,40 @@ const URLManagement = () => {
     }
   };
 
-  console.log({ websocketInstance, socketData, isConnected });
+  console.log({
+    connectionStatus,
+    getWebSocket,
+    lastJsonMessage,
+    jobID,
+    socketData,
+  });
   return (
     <Box>
-      <Typography variant="h4">URL Management</Typography>
+      <Stack direction={"row"} alignItems={"center"} gap={1}>
+        <Box>
+          {connectionStatus === "Connecting" || connectionStatus === "Open" ? (
+            <Tooltip title="Real Time Updates Possible">
+              <Icon
+                icon={"tabler:plug-connected"}
+                color="#2DB010"
+                fontSize={"2rem"}
+              />
+            </Tooltip>
+          ) : (
+            <Tooltip title="Real Time Updates NOT Possible">
+              <Icon
+                icon={"tabler:plug-connected-x"}
+                color="#222222"
+                fontSize={"2rem"}
+              />
+            </Tooltip>
+          )}
+        </Box>
+        <Typography variant="h4">URL Management</Typography>
+      </Stack>
+
       <Box sx={{ mt: 4, mb: 2 }}>
         <URLForm onSubmitUrls={handleSubmitUrls} loading={isSubmitting} />
-      </Box>
-      <Box>
-        {isConnected ? (
-          <Icon
-            icon={"tabler:plug-connected"}
-            color="#2DB010"
-            fontSize={"2rem"}
-          />
-        ) : (
-          <Icon
-            icon={"tabler:plug-connected-x"}
-            color="#222222"
-            fontSize={"2rem"}
-          />
-        )}
       </Box>
     </Box>
   );
---
+++ src/hooks/useReactWebSocket.ts
@@ -0,0 +1,87 @@
+import { useEffect, useRef, useState } from "react";
+import { webSocketUrl } from "./socket";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import type { TUseWebSocketHookResponse } from "./types";
+
+type TConnectionStatus =
+  | "Connecting"
+  | "Open"
+  | "Closing"
+  | "Closed"
+  | "Uninstantiated";
+type TUseWSHook = TUseWebSocketHookResponse<unknown> & {
+  wsRef: WebSocket | null;
+  connectionStatus: TConnectionStatus;
+  socketData: WebSocket | null;
+};
+export const useReactWebSocket = () => {
+  const wsRef = useRef<WebSocket | null>(null);
+  const [socketData, setSocketData] = useState(null);
+  const {
+    sendMessage,
+    sendJsonMessage,
+    lastMessage,
+    lastJsonMessage,
+    readyState,
+    getWebSocket,
+  } = useWebSocket(webSocketUrl, {
+    onOpen: () => {
+      const socket = getWebSocket();
+      if (socket instanceof WebSocket) {
+        wsRef.current = socket;
+      } else {
+        wsRef.current = null;
+      }
+      console.log("WebSocket connection established");
+    },
+    onClose: () => console.log("WebSocket connection closed"),
+    shouldReconnect: () => true, // Attempt to reconnect on all close events
+    reconnectAttempts: 10,
+    reconnectInterval: 3000,
+    onMessage: (event: WebSocketEventMap["message"]) => {
+      console.log("message event ", { liveEvent: event });
+      onEvent(event);
+    },
+  });
+
+  const onEvent = (event: MessageEvent) => {
+    console.log({ event });
+    const eventData = JSON.parse(event.data);
+
+    if (eventData.type === "job_update") {
+      setSocketData(eventData?.data);
+      console.log({ eventData });
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+        wsRef.current.close();
+      }
+    };
+  }, []);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState] as TConnectionStatus;
+
+  const useWebSocketHookValues: TUseWSHook = {
+    sendJsonMessage,
+    sendMessage,
+    lastMessage,
+    lastJsonMessage,
+    getWebSocket,
+    wsRef: wsRef.current,
+    connectionStatus,
+    socketData,
+  };
+
+  return {
+    ...useWebSocketHookValues,
+  };
+};
